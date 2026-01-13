8b56192a697cdb9c9c557ea374d26f98b7be76ec
superpank228-cloud <superpank228@gmail.com>
2026-01-13 20:23:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,19 +9,19 @@ BOT_TOKEN = "8429990932:AAE5criYIBQu4eJ6WapbQFpL7p4HhBsrGJ8"
 
 USER_MESSAGE =  Добро пожаловать в наш канал!
 
-✅ Ваша заявка одобрена автоматически.
+ Ваша заявка одобрена автоматически.
 
-📋 Что у нас есть:
+ Что у нас есть:
 • Отборный и ежедневный контент
 • Чат для общения и предложений
 
-💡 Подписывайтесь на наши другие ресурсы:
-👉 Канал 2: скоро будет!
-👉 Группа: https://t.me/+gTDkVdfPWbkwZGNi
+ Подписывайтесь на наши другие ресурсы:
+ Канал 2: скоро будет!
+ Группа: https://t.me/+gTDkVdfPWbkwZGNi
 
-📢 Для связи: mode_dibil@mail.ru
+ Для связи: mode_dibil@mail.ru
 
-Спасибо что с нами! 🚀
+Спасибо что с нами! 
 
 logging.basicConfig(level=logging.INFO)
 
@@ -37,15 +37,15 @@ async def auto_approve_and_message(update: Update, context: ContextTypes.DEFAULT
 
     await context.bot.send_message(
         chat_id=user.id,
-        text=f"👋 Привет, {user.full_name}!\n\n{USER_MESSAGE}"
+        text=f" Привет, {user.full_name}!\n\n{USER_MESSAGE}"
     )
 
-    print(f"✅ {user.full_name} принят")
+    print(f" {user.full_name} принят")
 
 async def main():
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(ChatJoinRequestHandler(auto_approve_and_message))
-    print("🤖 Бот запущен и ждёт заявки...")
+    print(" Бот запущен и ждёт заявки...")
     await app.run_polling()
 
 if __name__ == "__main__":
